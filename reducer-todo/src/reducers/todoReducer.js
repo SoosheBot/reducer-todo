1bd1000409a44591fd40d171b5ecfe3bcd4f3e92
sooshe <sooshe@sooshebot.com>
2020-01-21 14:32:13 -0600

    rename Todo--TodoForm+add initialState to reducer file

--- reducer-todo/src/reducers/todoReducer.js
+++ reducer-todo/src/reducers/todoReducer.js
@@ -1,7 +1,11 @@
-export const todo = [
+export const initialState = [
     {
         item: 'Learn about reducers and stuff',
         completed: false,
         id: 3892987589   
     }
 ];
+
+export const todoReducer = (state,action) => {
+    
+}
